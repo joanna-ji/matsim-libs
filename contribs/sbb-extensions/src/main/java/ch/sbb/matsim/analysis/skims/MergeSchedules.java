629430bbf57c86cafa1b7f8e119aab20c4cf88c7
Joanna Ji <joanna.ji@tum.de>
2024-06-03 15:52:51 +0200

    added qin's code to filter schedule by mode

---
+++ contribs/sbb-extensions/src/main/java/ch/sbb/matsim/analysis/skims/MergeSchedules.java
@@ -0,0 +1,216 @@
+package ch.sbb.matsim.analysis.skims;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.transitSchedule.TransitScheduleFactoryImpl;
+import org.matsim.pt.transitSchedule.api.*;
+import org.matsim.vehicles.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MergeSchedules {
+
+    public static void main(String[] args) {
+
+        /**
+         * initialize n scenarios
+         */
+        Scenario scenarioOne = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        Scenario scenarioTwo = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+//        Scenario scenarioThree = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        //Scenario scenarioFour= ScenarioUtils.createScenario(ConfigUtils.createConfig());
+
+        /*
+        Add here the n schedules to merge (if more or less, some minor changes in the code may be required)
+         */
+        new TransitScheduleReader(scenarioOne).readFile("./output/rb/schedule.xml");
+        new TransitScheduleReader(scenarioTwo).readFile("./output/db/schedule.xml");
+//        new TransitScheduleReader(scenarioThree).readFile("./output/blablabus/scheduleModeLdBus.xml");
+        //new TransitScheduleReader(scenarioFour).readFile("./output/flixbus/schedule.xml");
+        TransitSchedule scheduleOne = scenarioOne.getTransitSchedule();
+        TransitSchedule scheduleTwo = scenarioTwo.getTransitSchedule();
+//        TransitSchedule scheduleThree = scenarioThree.getTransitSchedule();
+        //TransitSchedule scheduleFour = scenarioFour.getTransitSchedule();
+
+        final TransitSchedule mergedSchedule = new TransitScheduleFactoryImpl().createTransitSchedule();
+
+        /**
+         * Define suffixes to the schedule elements to prevent duplicate ids
+         */
+        scheduleOne = addSuffixToScheduleElements(scheduleOne, "_rb");
+        scheduleTwo = addSuffixToScheduleElements(scheduleTwo, "_ld");
+//        scheduleThree = addSuffixToScheduleElements(scheduleThree, "_blablabus");
+        //scheduleFour = addSuffixToScheduleElements(scheduleFour, "_flixbus");
+
+        List<TransitSchedule> transitSchedules = new ArrayList<>();
+
+        transitSchedules.add(scheduleOne);
+        transitSchedules.add(scheduleTwo);
+//        transitSchedules.add(scheduleThree);
+        //transitSchedules.add(scheduleFour);
+
+        for (TransitSchedule schedule : transitSchedules){
+            for (TransitStopFacility stop : schedule.getFacilities().values()) {
+                mergedSchedule.addStopFacility(stop);
+            }
+
+            for (TransitLine line : schedule.getTransitLines().values()) {
+                mergedSchedule.addTransitLine(line);
+            }
+        }
+
+        /**
+         * Define output files for schedule and vehicles
+         */
+        new TransitScheduleWriter(mergedSchedule).writeFile("./output/ld_rb/schedule_germany.xml");
+
+        Vehicles vehicles = VehicleUtils.createVehiclesContainer();
+        createVehiclesForSchedule(mergedSchedule, vehicles);
+        new VehicleWriterV1(vehicles).writeFile("./output/ld_rb/vehicles_germany.xml");
+
+
+    }
+
+    public static void createVehiclesForSchedule(TransitSchedule schedule, Vehicles vehicles) {
+        VehiclesFactory vb = vehicles.getFactory();
+        Map<String, VehicleType> vehicleTypeMap = new HashMap<>();
+
+        //todo read from vehicle xml file can be better
+        {
+            VehicleType vehicleType = vb.createVehicleType(Id.create("default", VehicleType.class));
+            //VehicleCapacity capacity = new VehicleCapacityImpl();
+            vehicleType.getCapacity().setSeats(Integer.valueOf(50));
+            vehicleType.getCapacity().setStandingRoom(Integer.valueOf(50));
+            //vehicleType.setCapacity(capacity);
+            vehicles.addVehicleType(vehicleType);
+            vehicleTypeMap.put(vehicleType.getId().toString(), vehicleType);
+        }
+
+        {
+            VehicleType vehicleType = vb.createVehicleType(Id.create("bus", VehicleType.class));
+            //VehicleCapacity capacity = new VehicleCapacityImpl();
+            vehicleType.getCapacity().setSeats(Integer.valueOf(100));
+            vehicleType.getCapacity().setStandingRoom(Integer.valueOf(0));
+            //vehicleType.setCapacity(capacity);
+            vehicles.addVehicleType(vehicleType);
+            vehicleTypeMap.put(vehicleType.getId().toString(), vehicleType);
+        }
+
+        {
+            VehicleType vehicleType = vb.createVehicleType(Id.create("tram", VehicleType.class));
+            //VehicleCapacity capacity = new VehicleCapacityImpl();
+            vehicleType.getCapacity().setSeats(Integer.valueOf(100));
+            vehicleType.getCapacity().setStandingRoom(Integer.valueOf(100));
+            //vehicleType.setCapacity(capacity);
+            vehicles.addVehicleType(vehicleType);
+            vehicleTypeMap.put(vehicleType.getId().toString(), vehicleType);
+        }
+
+
+        {
+            VehicleType vehicleType = vb.createVehicleType(Id.create("subway", VehicleType.class));
+            //VehicleCapacity capacity = new VehicleCapacityImpl();
+            vehicleType.getCapacity().setSeats(Integer.valueOf(300));
+            vehicleType.getCapacity().setStandingRoom(Integer.valueOf(300));
+           //vehicleType.setCapacity(capacity);
+            vehicles.addVehicleType(vehicleType);
+            vehicleTypeMap.put(vehicleType.getId().toString(), vehicleType);
+        }
+
+        {
+            VehicleType vehicleType = vb.createVehicleType(Id.create("rail", VehicleType.class));
+            //VehicleCapacity capacity = new VehicleCapacityImpl();
+            vehicleType.getCapacity().setSeats(Integer.valueOf(300));
+            vehicleType.getCapacity().setStandingRoom(Integer.valueOf(0));
+            //vehicleType.setCapacity(capacity);
+            vehicles.addVehicleType(vehicleType);
+            vehicleTypeMap.put(vehicleType.getId().toString(), vehicleType);
+        }
+
+        for (TransitLine line : schedule.getTransitLines().values()) {
+            System.out.println(line.getId());
+            for (TransitRoute route : line.getRoutes().values()) {
+                System.out.println(route.getStops());
+                for (Departure departure : route.getDepartures().values()) {
+                    System.out.println(route.getDepartures().values().toString());
+                    Id<Vehicle> vehicleId = departure.getVehicleId();
+                    VehicleType vehicleType = getVehicleTypeFromId(vehicleId, vehicleTypeMap);
+                    Vehicle veh = vb.createVehicle(vehicleId, vehicleType);
+                    vehicles.addVehicle(veh);
+                    departure.setVehicleId(veh.getId());
+                }
+            }
+        }
+
+
+
+    }
+
+    private static VehicleType getVehicleTypeFromId(Id<Vehicle> vehicleId, Map<String, VehicleType> vehicleTypeMap) {
+        String string = vehicleId.toString().toLowerCase();
+        if (string.contains("bus")){
+            return vehicleTypeMap.get("bus");
+        } else if (string.contains("tram")){
+            return vehicleTypeMap.get("tram");
+        } else if (string.contains("subway")){
+            return vehicleTypeMap.get("subway");
+        } else if (string.contains("rail")){
+            return vehicleTypeMap.get("rail");
+        } else {
+            System.out.println("A default vehicle type is selected!");
+            return vehicleTypeMap.get("default");
+        }
+    }
+
+    private static TransitSchedule addSuffixToScheduleElements(TransitSchedule schedule, String suffix) {
+        TransitScheduleFactory factory = schedule.getFactory();
+        TransitSchedule newSchedule = factory.createTransitSchedule();
+        for (TransitStopFacility stop : schedule.getFacilities().values()) {
+            Id<TransitStopFacility> newStopId = Id.create(stop.getId() + suffix, TransitStopFacility.class);
+            TransitStopFacility newStop = factory.createTransitStopFacility(newStopId, stop.getCoord(), stop.getIsBlockingLane());
+            newSchedule.addStopFacility(newStop);
+        }
+
+        for (TransitLine line : schedule.getTransitLines().values()) {
+            Id<TransitLine> newLineId = Id.create(line.getId() + suffix, TransitLine.class);
+            TransitLine newLine = factory.createTransitLine(newLineId);
+
+            for (TransitRoute route : line.getRoutes().values()) {
+
+                List<TransitRouteStop> newStops = new ArrayList<>();
+
+                for (TransitRouteStop routeStop : route.getStops()) {
+                    Id<TransitStopFacility> newStopFacilityId = Id.create(routeStop.getStopFacility().getId() + suffix, TransitStopFacility.class);
+                    TransitStopFacility newStopFacility = newSchedule.getFacilities().get(newStopFacilityId);
+                    TransitRouteStop newRouteStop = factory.createTransitRouteStop(newStopFacility, routeStop.getArrivalOffset(), routeStop.getDepartureOffset());
+                    newStops.add(newRouteStop);
+                }
+
+
+                Id<TransitRoute> newRouteId = Id.create(route.getId() + suffix, TransitRoute.class);
+                TransitRoute newRoute = factory.createTransitRoute(newRouteId, null, newStops, route.getTransportMode());
+
+                for (Departure departure : route.getDepartures().values()) {
+                    departure.getDepartureTime();
+                    Id<Departure> newDepartureId = Id.create(departure.getId() + suffix, Departure.class);
+                    Id<Vehicle> newVehicleId = Id.create(departure.getVehicleId() + suffix, Vehicle.class);
+                    Departure newDeparture = factory.createDeparture(newDepartureId, departure.getDepartureTime());
+                    newDeparture.setVehicleId(newVehicleId);
+                    newRoute.addDeparture(newDeparture);
+                }
+
+                newLine.addRoute(newRoute);
+            }
+            newSchedule.addTransitLine(newLine);
+        }
+        return newSchedule;
+    }
+
+
+}
+
--- contribs/sbb-extensions/src/main/java/ch/sbb/matsim/analysis/skims/RunSkimMatrices.java
+++ contribs/sbb-extensions/src/main/java/ch/sbb/matsim/analysis/skims/RunSkimMatrices.java
@@ -5,6 +5,7 @@ import org.matsim.api.core.v01.Coord;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.utils.misc.Time;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
 
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -16,61 +17,67 @@ import java.util.Random;
 
 public class RunSkimMatrices {
 
-		public static void main(String[] args) throws IOException {
-			// Define configuration and input data paths
+	public static void main(String[] args) throws IOException {
+		// Define configuration and input data paths
 
-			String networkFilename = "\\\\nas.ads.mwn.de\\tubv\\mob\\indiv\\joanna\\munich_skim\\muc/studyNetworkDense_inclPT.xml";
+		String networkFilename = "\\\\nas.ads.mwn.de\\tubv\\mob\\projects\\2021\\tengos\\data\\skims\\input\\studyNetworkDense_inclPT.xml";
+		String transitScheduleFilename = "\\\\nas.ads.mwn.de\\tubv\\mob\\projects\\2021\\tengos\\data\\skims\\input\\qin_code\\bus_schedule.xml";
+		String samplePointsFilename = "\\\\nas.ads.mwn.de\\tubv\\mob\\projects\\2021\\tengos\\data\\skims\\input/sample_points_reset.csv";
+		String zonesShapeFilename = "\\\\nas.ads.mwn.de\\tubv\\mob\\projects\\2021\\tengos\\data\\mitoMunich_shapefiles\\zone/zonesNew.shp";
+		String eventsFilename= null;
+		String zonesIdAttributeName = "id";
 
-			// \\nas.ads.mwn.de\tubv\mob\indiv\joanna\munich_skim\muc/mapped_schedule_deutschland_20240225.xml
-			String transitScheduleFilename = "\\\\nas.ads.mwn.de\\tubv\\mob\\indiv\\joanna\\munich_skim\\muc/mapped_schedule_deutschland_20240225.xml";
-			String samplePointsFilename = "C:/models/skim_generation/muc/sample_points_reset.csv";
-			String zonesShapeFilename = "\\\\nas.ads.mwn.de\\tubv\\mob\\projects\\2021\\tengos\\data\\mitoMunich_shapefiles\\zone/zonesNew.shp";
-			String eventsFilename= null;
-			String zonesIdAttributeName = "id";
+		String outputDirectory = "\\\\nas.ads.mwn.de\\tubv\\mob\\projects\\2021\\tengos\\data\\skims/output_folder_bus3";
 
-			String outputDirectory = "C:/models/skim_generation/muc/output_folder_test";
-			int numberOfThreads = 8;
-			int numberOfPointsPerZone = 1;
+		int numberOfThreads = 8;
+		int numberOfPointsPerZone = 1;
 
-			String[] timesCarStr ={};
-			String[] timesPtStr = {"11:00", "11:10"};
+		String[] timesCarStr ={};
+		String[] timesPtStr = {"11:00", "11:10"};
 
-			double[] timesCar = new double[timesCarStr.length];
-			for (int i = 0; i < timesCarStr.length; i++) {
-				timesCar[i] = Time.parseTime(timesCarStr[i]);
-			}
-
-			double[] timesPt = new double[timesPtStr.length];
-			for (int i = 0; i < timesPtStr.length; i++) {
-				timesPt[i] = Time.parseTime(timesPtStr[i]);
-			}
-
-			Config config = ConfigUtils.createConfig();
-			Random r = new Random(4711);
-
-			// Initialize CalculateSkimMatrices with the output directory and number of threads
-			CalculateSkimMatrices skims = new CalculateSkimMatrices(outputDirectory, zonesIdAttributeName, outputDirectory,numberOfThreads);
-
-   			// Load sampling points from an existing file instead of calculating them
-
-			//skims.loadSamplingPointsFromFile(samplePointsFilename);
-
-			// Example place where Coord objects are used to find the nearest node or link
-
-
-			skims.calculateSamplingPointsPerZoneFromNetwork(networkFilename, numberOfPointsPerZone,zonesShapeFilename,zonesIdAttributeName, r);
-
-			// Calculate and write network matrices
-			if (timesCar.length > 0) {
-				skims.calculateAndWriteNetworkMatrices(networkFilename, eventsFilename, timesCar, config, "", link -> true);
-			}
-
-			// Calculate and write PT matrices
-			if (timesPt.length > 0) {
-				skims.calculateAndWritePTMatrices(networkFilename, transitScheduleFilename, timesPt[0], timesPt[1], config, "", (line, route) -> route.getTransportMode().equals("train"));
-			}
-
+		double[] timesCar = new double[timesCarStr.length];
+		for (int i = 0; i < timesCarStr.length; i++) {
+			timesCar[i] = Time.parseTime(timesCarStr[i]);
 		}
+
+		double[] timesPt = new double[timesPtStr.length];
+		for (int i = 0; i < timesPtStr.length; i++) {
+			timesPt[i] = Time.parseTime(timesPtStr[i]);
+		}
+
+		Config config = ConfigUtils.createConfig();
+		Random r = new Random(4711);
+
+		// Initialize CalculateSkimMatrices with the output directory and number of threads
+		CalculateSkimMatrices skims = new CalculateSkimMatrices(outputDirectory, zonesIdAttributeName, outputDirectory,numberOfThreads);
+
+		// Load sampling points from an existing file instead of calculating them
+
+		skims.loadSamplingPointsFromFile(samplePointsFilename);
+
+		// Example place where Coord objects are used to find the nearest node or link
+
+		//skims.calculateSamplingPointsPerZoneFromNetwork(networkFilename, numberOfPointsPerZone,zonesShapeFilename,zonesIdAttributeName, r);
+
+		// Calculate and write network matrices
+         /*if (timesCar.length > 0) {
+            skims.calculateAndWriteNetworkMatrices(networkFilename, eventsFilename, timesCar, config, "", link -> true);
+         }*/
+
+		// Calculate and write PT matrices
+
+
+		if (timesPt.length > 0) {
+			skims.calculateAndWritePTMatrices(networkFilename, transitScheduleFilename, timesPt[0], timesPt[1], config, "", (line, route) -> isRailTramOrSubway(route));
+		}
+	}
+
+	private static boolean isRailTramOrSubway(TransitRoute route) {
+		return
+				route.getTransportMode().equalsIgnoreCase("bus");
+	}
+
+
 
 
 }
